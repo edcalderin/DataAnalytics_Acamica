9729f793f947eead04ebe148c83cabcb6a7f4ffd
erick.calderin <edcm.erick@gmail.com>
2021-04-09 22:58:08 -0300

    ejercicios CRUD cars

---
+++ Meeting_4/ejercicios.sql
@@ -0,0 +1,30 @@
+-- Query 1: ¿Cuales son los modelos de auto con mayor peso promedio?¿Y mayor horsepower?
+
+SELECT
+	model_year	
+	, avg(weight) AS avg_col
+	--, avg(horsepower) AS avg_col	
+FROM
+	public.cars
+GROUP BY model_year
+ORDER BY avg_col DESC
+
+/*
+Query 2: Si realizas el mismo procedimiento pero en base a la aceleración. 
+¿Puedes sacar alguna relación entre lo que ves aquí y el peso promedio?
+*/
+SELECT
+	model_year	
+	, avg(acceleration) AS avg_acce	
+FROM
+	public.cars
+GROUP BY model_year
+ORDER BY avg_acce DESC
+
+/* 
+
+CONCLUSIONES:
+
+Query 1: Los primeros modelos tienden a tener menor peso
+Query 2: Los ultimos modelos tienden a tener una mayor aceleracion
+*/
